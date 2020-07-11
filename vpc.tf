75a902070d48c8c8a29713a2f5a558cbeac61f4e
vamshireddy07 <63277553+vamshireddy07@users.noreply.github.com>
2020-07-11 22:40:17 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -3,41 +3,40 @@ provider "aws" {
   region = "ap-south-1"
 }
 # VPC
-resource "aws_vpc" "vpc" {
+resource "aws_vpc" "kubernetes" {
   cidr_block = var.vpc_cidr
-  enable_dns_hostnames = true
   tags = {
-    Name = "demo_vpc"
+    Name = "kubernetes"
   }
 }
-# IGW for demo_vpc
-resource "aws_internet_gateway" "vpc_igw" {
-  vpc_id = aws_vpc.vpc.id
+# IGW for kubernetes_vpc
+resource "aws_internet_gateway" "kubernetes_vpc_igw" {
+  vpc_id = aws_vpc.kubernetes.id
   tags = {
-    Name = "demo_vpc_igw"
+    Name = "kubernetes_vpc_igw"
   }
 }
 
-# Subenets in demo_vpc
-resource "aws_subnet" "subnets" {
+# Subenets in kubernetes_vpc
+resource "aws_subnet" "kubernetes_subnets" {
   count                   = length(var.subnets_cidr)
-  vpc_id                  = aws_vpc.vpc.id
+  vpc_id                  = aws_vpc.kubernetes.id
   cidr_block              = element(var.subnets_cidr, count.index)
   availability_zone       = element(var.availability_zones, count.index)
   map_public_ip_on_launch = true
   tags = {
-    Name = "demo_vpc_subnet_${count.index + 1}"
+    Name = "kubernetes_subnets_${count.index + 1}"
   }
 }
-# Route table for demo_vpc
-resource "aws_route_table" "public_rt" {
-  vpc_id = aws_vpc.vpc.id
+# Route table for kubernetes_vpc
+resource "aws_route_table" "kubernetes_public_rt" {
+  vpc_id = aws_vpc.kubernetes.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.vpc_igw.id
+    gateway_id = aws_internet_gateway.kubernetes_igw.id
   }
   tags = {
-    Name = "demo_vpc_public_rt"
+    Name = "kubernetes_vpc_public_rt"
   }
 }
 
@@ -45,5 +44,5 @@ resource "aws_route_table" "public_rt" {
 resource "aws_route_table_association" "rt_sub_association" {
   count          = length(var.subnets_cidr)
   subnet_id      = element(aws_subnet.subnets.*.id, count.index)
-  route_table_id = aws_route_table.public_rt.id
+  route_table_id = aws_route_table.kubernetes_public_rt.id
 }
