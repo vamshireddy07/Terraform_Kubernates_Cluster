2b1f05508056b4c798f801a37e439410045492d5
vamshireddy07 <63277553+vamshireddy07@users.noreply.github.com>
2020-07-11 22:36:01 +0530

    Update Instances.tf

--- Instances.tf
+++ Instances.tf
@@ -1,28 +1,29 @@
-resource "aws_instance" "Demo_Server" {
+resource "aws_instance" "kubernetes_Server" {
   count                  = 1
-  ami                    = var.ubuntuami
-  instance_type          = "t2.medium"
-  vpc_security_group_ids = [aws_security_group.demo_sg.id]
-  subnet_id              = element(aws_subnet.subnets.*.id, count.index)
+  ami                    = var.kubernetes_ami
+  instance_type          = var.master_instance_type
+  vpc_security_group_ids = [aws_security_group.kubernetes_sg.id]
+  subnet_id              = element(aws_subnet.kubernetes_subnets.*.id, count.index)
   key_name               = var.key_name
 
   tags = {
-    Name = "UbuntuServer"
-    Type = "Docker"
+    Name = "kubernetes_Server"
+    Type = "kubernetes_master"
   }
 
 }
 # 2 Instance Of Redhat Servers(t2.micro)
-resource "aws_instance" "redhat_servers" {
+resource "aws_instance" "kubernetes_workers" {
   count                  = 2
-  ami                    = var.redhatami
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.demo_sg.id]
-  subnet_id              = element(aws_subnet.subnets.*.id, count.index)
+  ami                    = var.kubernetes_ami
+  instance_type          = var.master_instance_type
+  vpc_security_group_ids = [aws_security_group.kubernetes_sg.id]
+  subnet_id              = element(aws_subnet.kubernetes_subnets.*.id, count.index)
   key_name               = var.key_name
-  user_data              = file("init_script.sh")
+ 
   tags = {
-    Name = "Redhat_Server_${count.index + 1}"
+    Name = "kubernetes_Server"
+    Type = "kubernetes_master"
   }
 
 
