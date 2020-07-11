e89c7ce21244586e933d42e901ee01728d56efa5
vamshireddy07 <63277553+vamshireddy07@users.noreply.github.com>
2020-07-11 22:29:21 +0530

    Update security-groups.tf

--- security-groups.tf
+++ security-groups.tf
@@ -1,7 +1,7 @@
-resource "aws_security_group" "demo_sg" {
+resource "aws_security_group" "kubernetes_sg" {
   name        = "Allow_All_Ports"
   description = "Allow All Ports All Protocals"
-  vpc_id      = aws_vpc.vpc.id
+  vpc_id      = aws_vpc.kubernetes.id
   ingress {
     from_port   = 0
     to_port     = 0
