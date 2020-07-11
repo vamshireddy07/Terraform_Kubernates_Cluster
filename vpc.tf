ebadfd2b9939b566e41e562a0232447c2e44f8a8
vamshireddy07 <63277553+vamshireddy07@users.noreply.github.com>
2020-07-11 23:08:32 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,15 +1,12 @@
-# Provider
 provider "aws" {
   region = "ap-south-1"
 }
-# VPC
 resource "aws_vpc" "kubernetes" {
   cidr_block = var.vpc_cidr
   tags = {
     Name = "kubernetes"
   }
 }
-# IGW for kubernetes_vpc
 resource "aws_internet_gateway" "kubernetes_vpc_igw" {
   vpc_id = aws_vpc.kubernetes.id
   tags = {
@@ -17,7 +14,6 @@ resource "aws_internet_gateway" "kubernetes_vpc_igw" {
   }
 }
 
-# Subenets in kubernetes_vpc
 resource "aws_subnet" "kubernetes_subnets" {
   count                   = length(var.subnets_cidr)
   vpc_id                  = aws_vpc.kubernetes.id
@@ -28,7 +24,7 @@ resource "aws_subnet" "kubernetes_subnets" {
     Name = "kubernetes_subnets_${count.index + 1}"
   }
 }
-# Route table for kubernetes_vpc
+
 resource "aws_route_table" "kubernetes_public_rt" {
   vpc_id = aws_vpc.kubernetes.id
   route {
@@ -40,7 +36,7 @@ resource "aws_route_table" "kubernetes_public_rt" {
   }
 }
 
-# Route table and subnets assocation
+
 resource "aws_route_table_association" "rt_sub_association" {
   count          = length(var.subnets_cidr)
   subnet_id      = element(aws_subnet.kubernetes_subnets.*.id, count.index)
